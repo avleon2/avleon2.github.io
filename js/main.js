8416de2878399fb8b0ce33c94b35097a84949f0a
Tito <avleon83@gmail.com>
2022-07-19 23:33:37 -0400

    pic correction

--- js/main.js
+++ js/main.js
@@ -26,29 +26,61 @@ var i = 0;
 
   window.onload = changeImg;
 
-//RadioButton Selection
-
-/*function check_value() {
-  switch(#podPic) {
-    //1
-    document.getElementById(#radio1) = "<images/HOLDER.jpg>";
-  }
-}
-*/
-
-
 //Close Modal
 const modal = document.querySelector('.modalCover');
-const closeModalBtn = document.querySelector('#closeModal');
 const cover = document.querySelector('.modalBox');
+const closeModalBtn = document.querySelector('#closeModal');
 
 closeModalBtn.addEventListener('click', modalClose);
 
 function modalClose(){
-    modal.classList.remove('modalCover');
-    cover.classList.remove('modalBox');
-    closeModalBtn.style.display = 'none';
-    cover.style.display = 'none';
+  modal.classList.remove('modalCover');
+  cover.classList.remove('modalBox');
+  closeModalBtn.style.display = 'none';
+  cover.style.display = 'none';
 }
 
-//$("#myModal").modal('hide');
+//Newsletter
+
+/*const newsletter = document.getElementbyId('#appear');
+const openNewsletterBtn = document.getElementbyId('#show');
+
+openNewsletterBtn.addEventListener('click', newsletterAppear);
+
+function newsletterAppear(){
+  newsletter.classList.display('appear');
+};
+
+
+
+
+//RadioButton Selection
+
+
+/*var i = 0;
+var images = [];
+
+images[0] = 'images/PACEHOLDER.jpg';
+images[1] = 'images/PACEHOLDER.jpg';
+images[2] = 'images/PACEHOLDER.jpg';
+....
+images[23] = 'images/PACEHOLDER.jpg';
+
+function check_value() {
+  document.getElementIdBy("radio1.selected")
+    //1
+    document.getElementById(#radio1) = "<images/HOLDER.jpg>";
+  }
+} 
+const bonds = {
+   bond1: [pic1, pic2, pic3, pic4],
+   bond2: [pic1, pic2, pic2, pic4]
+}
+event.lookUpId
+
+let selectedBond = evt.target.id
+bonds[selectedBond]
+
+bonds[selectedBond]
+is equivalent to bonds['bond1']
+*/
